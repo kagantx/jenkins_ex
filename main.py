c9a1d7a8baa536a4caf1c47ba5978098707dce19
kagantx <kagantx@gmail.com>
2020-06-14 18:22:45 +0300

    ex3

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 from time import sleep
 import requests
 if __name__ == "__main__":
-    my_urls = join(sys.argv[1:]).split(',')
+    my_urls = (sys.argv[1:]).split(',')
     for url in my_urls:
         data=requests.get(url)
         print(data.content)
