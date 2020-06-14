64b04f167a5556b818aa976969c7cdf45d7d77fb
kagantx <66307840+kagantx@users.noreply.github.com>
2020-06-14 15:14:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import sys
 def main():
-  sys.stdout.flush()
-  my_input=int(sys.argv[1])
-  for num in my_input:
-    print(num, num*num)
+    my_input=int(sys.argv[1])
+    for num in my_input:
+        print(num, num*num)
+main()
 
   
