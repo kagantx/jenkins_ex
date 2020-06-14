094e92ca1e56f649de00bcd7cac1ac6856e28215
kagantx <kagantx@gmail.com>
2020-06-14 17:06:33 +0300

    Add Dockerfile

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import sys
-if __name__=="__main__":
-    my_input=int(sys.argv[1])
+from time import sleep
+
+if __name__ == "__main__":
+    my_input = int(sys.argv[1])
     for num in range(my_input):
-        print(num, num*num)
-
-
-  
+        print(num, num * num)
+        sleep(1)
