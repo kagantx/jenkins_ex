a577cee2d0ecc3392ae9c9d5e774826d63d8c853
kagantx <kagantx@gmail.com>
2020-06-15 14:50:00 +0300

    ex3

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ FROM python:3.6-jessie
 WORKDIR /opt
 ADD / /opt
 RUN pip install -r requirements.txt
-ENTRYPOINT python -u /opt/main.py MYVAR
+ENTRYPOINT python -u /opt/main.py
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import sys
+import os
 from time import sleep
 import requests
 if __name__ == "__main__":
-    my_urls = (sys.argv[1]).split(',')
+    my_urls = os.environment[0].split(',')
     print(my_urls)
     for url in my_urls:
         data=requests.get(url)
