d6b029e2a56dad0d9ca001e56d796b120f6c7194
kagantx <kagantx@gmail.com>
2020-06-15 15:11:24 +0300

    ex3

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
     urls = os.environ['MYVAR']
     my_urls=urls.split(',')
     for url in my_urls:
+        print(url)
         data=requests.get(url)
         print(data.content)
         sleep(1)
