99782dcd08a56d1751119767088434a42fd83f9d
kagantx <kagantx@gmail.com>
2020-06-15 15:02:20 +0300

    ex3

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from time import sleep
 import requests
 if __name__ == "__main__":
-    my_urls = os.environ['MYVAR'].split(',')
+    my_urls = os.environ['MYVAR']
     print(my_urls)
     for url in my_urls:
         data=requests.get(url)
